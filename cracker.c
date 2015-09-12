74d94343f5cef4c3240ef89d92931342ba0274d4
martich2 <martich2@ondi.orst.edu>
2015-09-12 04:06:19 -0700

    added function prototypes, change some too

--- cracker.c
+++ cracker.c
@@ -4,10 +4,15 @@
 #define START 0x0000000000000000L
 #define STOP  0x000000000FFFFFFFL
 
-int filter(uint64_t number);
-char* itoa(uint64_t number);
+int repeat_filter(uint64_t number, int repeats);
+int base_count_filter(uint64_t number, int count);
+void i2a(uint64_t number, char *str);
+void _reverse(char *str);
+
 int main(int argc, char **argv)
 {
+    char passwd_buffer[4*1024];
+
     uint64_t number = START;
 
     for (; number <= STOP; ++number)
